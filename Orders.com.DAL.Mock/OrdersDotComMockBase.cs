976fd2c6e8e2f9f751a244a6bd442daa3087f173
Aaron Hanusa <aaron.hanusa@gmail.com>
2015-10-25 10:02:08 -0500

    mock refactoring less concurrency support

---
+++ Orders.com.DAL.Mock/OrdersDotComMockBase.cs
@@ -0,0 +1,20 @@
+﻿using Peasy.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orders.com.DAL.Mock
+{
+    public abstract class OrdersDotComMockBase<DTO> : MockRepositoryBase<DTO, long> where DTO : IDomainObject<long>
+    {
+        protected override long GetNextID()
+        {
+            if (Data.Values.Any())
+                return Data.Values.Max(c => c.ID) + 1;
+
+            return 1;
+        }
+    }
+}
